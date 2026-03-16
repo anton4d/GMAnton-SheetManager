c9a5bc1403b4628b97451a1f2ae9921e6c5b3c93
Anton Christensen <53832748+anton4d@users.noreply.github.com>
2026-03-16 08:57:06 +0100

    Feature/#7 projects page (#22)
    * first look at the projects page
    
    - changed the homepage loadrepop into a specefik repo service
    - used the repo service in both homepage component and projects
    - created a type libary
    - changed so ng generate command will generate scss and not css
    - small changed in homepage html and errro where i used both a and button
    
    * changed the frontpage layout of projects
    - changed it to 3 in a collumn instead of 5

--- gmanton-sheetmanager/src/app/app.routes.ts
+++ gmanton-sheetmanager/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Portfolio } from './components/portfolio/portfolio'; 
 import { Homepage } from './components/portfolio/homepage/homepage';
 import { AboutPage } from './components/portfolio/about-page/about-page';
+import { Projects } from './components/portfolio/projects/projects';
 
 import { Sagamanager } from './components/sagamanager/sagamanager';
 import { SagaManagerHomepage } from './components/sagamanager/homepage/homepage';
@@ -14,7 +15,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: Homepage },
       { path: 'about', component: AboutPage },
-      { path: 'projects', component: Homepage },
+      { path: 'projects', component: Projects },
       { path: 'contact', component: Homepage },
     ]
   },{
--- gmanton-sheetmanager/src/app/components/portfolio/homepage/homepage.ts
+++ gmanton-sheetmanager/src/app/components/portfolio/homepage/homepage.ts
@@ -1,26 +1,9 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule, Star, Bug } from 'lucide-angular';
-import { environment } from '@env';
 
-interface Repo {
-  id: number;
-  name: string;
-  full_name: string;
-  description: string | null;
-  html_url: string;
-  topics: string[];
-  stargazers_count: number;
-  open_issues_count: number;
-  updated_at: string;
-  languages: string[];
-}
-
-interface RepoResponse {
-  success: boolean;
-  result: Repo[];
-}
+import { Repo } from '@Types';
+import { RepoService } from '../Services/repoService/repo-service';
 
 @Component({
   selector: 'portfolio-homepage',
@@ -28,43 +11,38 @@ interface RepoResponse {
   templateUrl: './homepage.html',
   styleUrls: ['./homepage.scss', '../portfolio.scss'],
 })
-export class Homepage implements OnInit {
+
+export class Homepage {
+  private repoService = inject(RepoService)
+
   readonly Star = Star;
   readonly Bug = Bug;
 
   repos = signal<Repo[]>([]);
-  error = signal(false);
+  error = signal(true);
   loading = signal(false);
-
-  constructor(private readonly http: HttpClient) {}
 
   ngOnInit(): void {
     this.loadRepos();
   }
 
-  loadRepos(): void {
-    this.repos.set([]);
-    this.error.set(false);
+  loadRepos() {
     this.loading.set(true);
+    this.error.set(false);
 
-    this.http.get<RepoResponse>(`${environment.apiUrl}/V1/PortFolio/repos`)
-      .subscribe({
-        next: (res) => {
-          if (!res.success) {
-            this.error.set(true);
-            this.loading.set(false);
-            return;
-          }
-          this.repos.set(res.result);
-          this.loading.set(false);
-        },
-        error: () => {
-          this.error.set(true);
-          this.loading.set(false);
-        },
-      }
-    );
+    this.repoService.GetFirstRepos().subscribe({
+      next: (repos) => {
+        this.repos.set(repos);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.error.set(true);
+        this.loading.set(false);
+      },
+    });
   }
+
+
 
   scrollToSection(id: string): void {
     document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
